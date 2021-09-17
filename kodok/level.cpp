bb2f094dc82b63c92fd2607debb0c51f54197eae
Ludman Viktor <ludmanviktor210@gmail.com>
2021-09-17 13:07:11 +0200

    upload

---
+++ kodok/level.cpp
@@ -0,0 +1,45 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Adj meg egy nevet!\n";
+	string nev;
+	cin >> nev;
+	cout <<"Dear, "<<nev<<"!\n";
+	cout << "How are you? I am fine. I miss you.\n";
+	string barat = "Károly";
+	cout << "Have you seen "<<barat<<" lately\n";
+	char nem = '0';
+	cout << "Add meg a barát nemét(m/f)\n";
+	cin >> nem;
+	if(nem='m')
+	{
+		cout << "If you see "<<barat<<" please ask him to call me.\n";
+	}
+	else if(nem='f')
+	{
+		cout << "If you see friend_name please ask her to call me.\n";
+	}
+	cout << "Add meg a barát korát!\n";
+	int kor;
+	cin >> kor;
+	if(kor<=0 || kor>110)
+	{
+		simple_error("you're kidding!");
+	}
+	else if(kor<12)
+	{
+		cout << "Next year you will be "<<kor+1.<<"\n";
+	}
+	else if(kor=17)
+	{ 
+		cout << "Next year you will be able to vote.";
+		}
+		else if(kor>=70)
+		{
+			cout << "Hope you are enjoying retirement.";
+			}
+			cout << "\n\nYours sincerely, Viktor\n";
+		
+}
+
